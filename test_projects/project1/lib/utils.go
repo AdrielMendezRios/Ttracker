7873ab531f07f6f685fdb89a74a7abf597d9d537
adrielmendezrios <adriel315@gmail.com>
2025-03-26 11:08:49 -0400

    Remove test_projects from git tracking

--- test_projects/project1/lib/utils.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-// TODO: Add documentation
-func Helper() string {
-	return "helper"
-	// TODO: Implement proper helper
-}
--- test_projects/project1/src/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-// TODO: Implement main function
-func main() {
-    fmt.Println("Project 1")
-    // FIXME: Add proper implementation
-}
